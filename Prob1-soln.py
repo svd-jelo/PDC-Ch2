12b4ef9f98f2fa6a46bb00014b9e2d10fb53e9dc
Christian Jelo Artoza <skulpayls21@gmail.com>
2022-08-27 15:54:53 +0800

    Updated code for revised solution

--- Prob1-soln.py
+++ Prob1-soln.py
@@ -14,17 +14,21 @@ def invL(F):
 #constants
 w1 = 500 #stream 1 flowrate (kg/min)
 w2 = 200 #stream 2 flowrate (kg/min)
-w = 700 #exit stream flowrate (kg/min)
 x1 = 0.4 #stream 1 composition (mass frac)
 x2 = 0.75 #stream 2 composition (mass frac)
 
-alpha1 = x2*w2/(p*V)
-alpha2 = x1*w1/(p*V)
-alpha3 = 0.2*w1/(p*V)
+w = w1 + w2 #exit stream flowrate (kg/min)
+p = 900 #density (kg/m3)
+V = 2 #liquid holdup (m3)
+tau = p*V/w #some constant
 
-beta1 = 100*x1/(p*V)
-beta2 = 100*x2/(p*V)
-beta3 = 100*x2/(p*V)
+alpha1 = x2*w2/w
+alpha2 = x1*w1/w
+alpha3 = 0.2*w1/w
+
+beta1 = 100*x1/w
+beta2 = 100*x2/w
+beta3 = 100*x2/w
 
 #inputs
 u1_ = alpha1 + beta1*(5-sp.Heaviside(t-15))
@@ -32,20 +36,20 @@ u2_ = alpha2 + beta2*(2-sp.Heaviside(t-15))
 u3_ = alpha3*(2 + sp.Heaviside(t-15)) + beta3*(2-sp.Heaviside(t-15))
 
 #initial conditions
-x01 = 0.0
-x02 = 0.0
-x03 = 0.0
+x01 = 0
+x02 = 0
+x03 = 0
 
 #Subsidiary Equation
-Q = 1/(s+1)
+Q = 1/(tau*s + 1)
 
 R1 = sp.laplace_transform(u1_,t,s,noconds=true)
 R2 = sp.laplace_transform(u2_,t,s,noconds=true)
 R3 = sp.laplace_transform(u3_,t,s,noconds=true)
 
-X1 = (x01 + R1)*Q
-X2 = (x02 + R2)*Q
-X3 = (x03 + R3)*Q
+X1 = (tau*x01 + R1)*Q
+X2 = (tau*x02 + R2)*Q
+X3 = (tau*x03 + R3)*Q
 
 #Solution
 x1_ = invL(X1)
@@ -53,12 +57,17 @@ x2_ = invL(X2)
 x3_ = invL(X3)
 
 #Time interval
-a = np.linspace(0,25,100)
+a = np.linspace(0,25,250)
 
-#Lambdified inputs
-u1 = lambdify(t, u1_, "numpy")
-u2 = lambdify(t, u2_, "numpy")
-u3 = lambdify(t, u3_, "numpy")
+#w1, w2, x
+w1_v = np.ones(len(a))*500
+w1_v[150:] = 400
+
+w2_v = np.ones(len(a))*200
+w2_v[150:] = 100
+
+x1_v = np.ones(len(a))*0.4
+x1_v[150:] = 0.6
 
 #Lambdified solutions
 x1 = lambdify(t, x1_, "numpy")
@@ -69,28 +78,30 @@ x3 = lambdify(t, x3_, "numpy")
 fig, ax = plt.subplots(nrows=3,ncols=2,figsize=(14,14))
 
 #row1
-ax[0,0].plot(a,u1(a),'r--',linewidth=3,label='input u(t)')
+ax[0,0].plot(a,w1_v,'r--',linewidth=2,label='input w1(t)')
+ax[0,0].set_ylabel('flowrate 1 (kg/min)')
 ax[0,0].legend()
 
-ax[0,1].plot(a,x1(a),'b-',linewidth=3,label='response x(t)')
+ax[0,1].plot(a,x1(a),'b-',linewidth=2,label='response x(t)')
 ax[0,1].legend()
 
 #row2
-ax[1,0].plot(a,u2(a),'g--',linewidth=3,label='input u(t)')
-ax[1,0].set_ylabel('input value')
+ax[1,0].plot(a,w2_v,'g--',linewidth=2,label='input w2(t)')
+ax[1,0].set_ylabel('flowrate 2 (kg/min)')
 ax[1,0].legend()
 
-ax[1,1].plot(a,x2(a),'b-',linewidth=3,label='response x(t)')
+ax[1,1].plot(a,x2(a),'b-',linewidth=2,label='response x(t)')
 ax[1,1].set_ylabel('composition of x (mass frac)')
 ax[1,1].legend()
 
 #row3
-ax[2,0].plot(a,u3(a),'y--',linewidth=3,label='input u(t)')
-ax[2,0].set_xlabel('time (s)')
+ax[2,0].plot(a,x1_v,'y--',linewidth=2,label='input x1(t)')
+ax[2,0].set_ylabel('composition 1 (mass frac)')
+ax[2,0].set_xlabel('time (min)')
 ax[2,0].legend()
 
-ax[2,1].plot(a,x3(a),'b-',linewidth=3,label='response x(t)')
-ax[2,0].set_xlabel('time (s)')
+ax[2,1].plot(a,x3(a),'b-',linewidth=2,label='response x(t)')
+ax[2,1].set_xlabel('time (min)')
 ax[2,1].legend()
 
 plt.show()
